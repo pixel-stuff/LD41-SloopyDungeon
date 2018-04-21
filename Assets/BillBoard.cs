defaa929cb70dabd86ad6256af85d6b0b2d6f001
Jesaipo <jeremy.morel.jm@gmail.com>
2018-04-22 00:24:11 +0200

    Add Billboard

---
+++ Assets/BillBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (-Camera.main.transform.position, Vector3.up);
+		//this.transform.forward = Camera.main.transform.forward;
+	}
+}
+
+

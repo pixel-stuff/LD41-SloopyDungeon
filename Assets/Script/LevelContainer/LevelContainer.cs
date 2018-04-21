cf736870f63bac732f4740e7be2b121c8b77d73e
Mathias <mussetmathias@gmail.com>
2018-04-22 00:37:59 +0200

    correction in plane rotation
    add max rotation
    real lock of values

--- Assets/Script/LevelContainer/LevelContainer.cs
+++ Assets/Script/LevelContainer/LevelContainer.cs
@@ -1,27 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 [RequireComponent(typeof(Rigidbody))]
 public class LevelContainer : MonoBehaviour {
 
   //UNITY VAR
+  [Header("Anchor")]
   [SerializeField] Transform upAnchor;
   [SerializeField] Transform downAnchor;
   [SerializeField] Transform leftAnchor;
   [SerializeField] Transform rightAnchor;
 
+  [Header("Anchor")]
+  [SerializeField] float yRotationLock = 25.0f;
+  [SerializeField] float maxRotation = 30.0f;
+
   // VAR
   Dictionary<InputDirection,Vector3> anchorPositions;
+  Vector3 rotationTemp;
 
 	/// <summary>
   /// Start this instance.
   /// </summary>
 	void Start () {
     anchorPositions = new Dictionary<InputDirection, Vector3>();
-    anchorPositions.Add(InputDirection.UP,upAnchor.position);
-    anchorPositions.Add(InputDirection.DOWN,downAnchor.position);
-    anchorPositions.Add(InputDirection.LEFT,leftAnchor.position);
-    anchorPositions.Add(InputDirection.RIGHT,rightAnchor.position);
+    anchorPositions.Add(InputDirection.UP,downAnchor.position);
+    anchorPositions.Add(InputDirection.DOWN,upAnchor.position);
+    anchorPositions.Add(InputDirection.LEFT,rightAnchor.position);
+    anchorPositions.Add(InputDirection.RIGHT,leftAnchor.position);
 
     InputManager.m_instance.onKeyBoardInputMaintain += handleInputMaintain;
     InputManager.m_instance.onKeyBoardInputEnter += handleInputEnter;
@@ -32,22 +39,44 @@ public class LevelContainer : MonoBehaviour {
   /// Handles the input that are held pressed.
   /// </summary>
   void handleInputEnter(InputDirection _direction){
-    GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up*4.0f,anchorPositions[_direction],ForceMode.Impulse);
+    var normal = transform.TransformDirection(GetComponent<MeshFilter>().mesh.normals[0]);
+    GetComponent<Rigidbody>().AddForceAtPosition(normal*7.0f,anchorPositions[_direction],ForceMode.Impulse);
   }
 	
   /// <summary>
   /// Handles the input that are held pressed.
   /// </summary>
   void handleInputMaintain(InputDirection _direction){
-    GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up*2.0f,anchorPositions[_direction],ForceMode.Impulse);
+    var normal = transform.TransformDirection(GetComponent<MeshFilter>().mesh.normals[0]);
+    GetComponent<Rigidbody>().AddForceAtPosition(normal*2.0f,anchorPositions[_direction],ForceMode.Impulse);
   }
 
   /// <summary>
   /// Update this instance.
   /// </summary>
-  //void Update() {
-    
-  //}
+  void Update() {
+    rotationTemp = transform.eulerAngles;
+
+    //block Y rotation
+    rotationTemp.y = yRotationLock;
+
+    //block X rotation if needed
+    if(rotationTemp.x > 180 && rotationTemp.x < (360 - maxRotation)){
+      rotationTemp.x = -maxRotation;
+    }else if(rotationTemp.x > maxRotation && rotationTemp.x <= 180){
+      rotationTemp.x = maxRotation;
+    }
+
+    //block z rotation if needed
+    if(rotationTemp.z > 180 && rotationTemp.z < (360 - maxRotation)){
+      rotationTemp.z = -maxRotation;
+    }else if(rotationTemp.z > maxRotation && rotationTemp.z <= 180){
+      rotationTemp.z = maxRotation;
+    }
+
+    transform.eulerAngles = rotationTemp;
+    transform.localPosition = Vector3.zero;
+  }
 
   /// <summary>
   /// Ons the destroy.

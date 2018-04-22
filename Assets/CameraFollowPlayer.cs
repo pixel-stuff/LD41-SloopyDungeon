9519aac5c13548a65535b88b757984be7e6529d1
Mathias <mussetmathias@gmail.com>
2018-04-22 16:03:55 +0200

    add Respawn tag
    work on Mathias Scene

---
+++ Assets/CameraFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+  // UNITY VAR
+  [SerializeField] Vector3 cameraPosition = new Vector3(-13.5f,6.4f,-0.5f);
+  [SerializeField] Vector3 cameraLocal = new Vector3(40.0f,60.0f,0.0f);
+
+  // VAR
+  Transform playerTransfrom;
+  Vector3 positionTemp;
+
+	// Use this for initialization
+	void Start () {
+    playerTransfrom =  GameObject.FindGameObjectWithTag("player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    positionTemp = transform.localPosition;
+
+	}
+}

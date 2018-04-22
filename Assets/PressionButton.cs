27b5eaeb9e310f6a5758e94d32e4433073c8c685
Jesaipo <jeremy.morel.jm@gmail.com>
2018-04-22 19:42:02 +0200

    Add pression

---
+++ Assets/PressionButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressionButton : MonoBehaviour {
+
+	[SerializeField] UnityEvent OnActivateButton;
+	[SerializeField] UnityEvent OnCoolDownOver;
+
+	private float CooldownSec;
+	private bool IsActivated = false;
+
+	void Update () {
+		if(IsActivated){
+			CooldownSec -= Time.deltaTime;
+			if (CooldownSec < 0) {
+				OnCoolDownOver.Invoke ();
+				IsActivated = false;
+			}
+		}
+	}
+
+	public void SetCooldown(float cooldown){
+		CooldownSec = cooldown;
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("TouchTheButton");
+		if (other.gameObject.tag == "player" || other.gameObject.tag == "HeavyObject") {
+			if (!IsActivated) {
+				OnActivateButton.Invoke ();
+				IsActivated = true;
+			}
+		}
+	}
+}

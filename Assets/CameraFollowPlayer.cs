5f66786b3f8fbd409feecd0caf8f26d8d71cd029
Mathias <mussetmathias@gmail.com>
2018-04-22 17:23:28 +0200

    creation of 2 level (short and long)

--- Assets/CameraFollowPlayer.cs
+++ Assets/CameraFollowPlayer.cs
@@ -2,21 +2,31 @@
 
 public class CameraFollowPlayer : MonoBehaviour {
   // UNITY VAR
-  [SerializeField] Vector3 cameraPosition = new Vector3(-13.5f,6.4f,-0.5f);
-  [SerializeField] Vector3 cameraLocal = new Vector3(40.0f,60.0f,0.0f);
+  //[SerializeField] Vector3 cameraPosition = new Vector3(-13.5f,6.4f,-0.5f);
+  //[SerializeField] Vector3 cameraLocal = new Vector3(40.0f,60.0f,0.0f);
 
   // VAR
-  Transform playerTransfrom;
-  Vector3 positionTemp;
+  //Transform playerTransfrom;
+  //Vector3 positionTemp;
+
+  Vector3 initialDistance;
+  Vector3 initialOrientation;
+  Vector3 initialCameraPosition;
+  Transform playerTransform;
 
 	// Use this for initialization
 	void Start () {
-    playerTransfrom =  GameObject.FindGameObjectWithTag("player").transform;
+   // playerTransfrom =  GameObject.FindGameObjectWithTag("player").transform;
+    playerTransform = GameObject.FindGameObjectWithTag("player").transform;
+
+    initialDistance = (playerTransform.position - transform.position);
+    initialOrientation = transform.eulerAngles;
+    initialCameraPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-    positionTemp = transform.localPosition;
+    transform.position = new Vector3(playerTransform.position.x - initialDistance.x,initialCameraPosition.y,playerTransform.position.z - initialDistance.z);
 
 	}
 }

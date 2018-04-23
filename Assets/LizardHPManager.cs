4580dfc8815deafe99541e85cdb5ed9937587b68
Jesaipo <jeremy.morel.jm@gmail.com>
2018-04-23 13:32:30 +0200

    HP on lizard ( max 3 )

---
+++ Assets/LizardHPManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LizardHPManager : MonoBehaviour {
+
+	public GameObject OneHP;
+	public GameObject TwoHP;
+	public GameObject ThreeHP;
+
+	public int lifeCount = 0;
+
+	public void OnTakeHit(){
+		DisplayHP(--lifeCount);
+	}
+	public void OnHeal(){
+		DisplayHP(++lifeCount);
+	}
+
+	public void DisplayHP(int currentHP){
+		if (currentHP == 1) {
+			OneHP.SetActive (true);
+			TwoHP.SetActive (false);
+			ThreeHP.SetActive (false);
+		} else if (currentHP == 2) {
+			OneHP.SetActive (false);
+			TwoHP.SetActive (true);
+			ThreeHP.SetActive (false);
+		} else if (currentHP == 3) {
+			OneHP.SetActive (false);
+			TwoHP.SetActive (false);
+			ThreeHP.SetActive (true);
+		} else {
+			OneHP.SetActive (false);
+			TwoHP.SetActive (false);
+			ThreeHP.SetActive (false);
+		}
+	}
+}
